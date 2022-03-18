9d004e69a506a808453d8251c6b8736a793de6ea
Daniel Loshkarev <81983874+danlosh@users.noreply.github.com>
2022-03-17 19:24:31 -0600

    Add toggle switch created to show form on/off

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,30 @@
 import PropTypes from "prop-types";
-import Button from "./Button"
+import Button from "./Button";
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Click')
-    }
-    return (
-        <header className="header">
-            <h1>{title}</h1>
-            <Button onClick={onClick} color='green' text="ADD" />
-           
-        </header>
-    )
-}
+const Header = ({ title, onAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button onClick={onAdd} color="green" text="ADD" />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-title: "Task Tracker",
-
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-title:PropTypes.string.isRequired,
-
-}
-
+  title: PropTypes.string.isRequired,
+};
 
 // How to do CSS in JS
 // const headingStyle = {
-//     color: 'blue', 
+//     color: 'blue',
 //     display: 'flex',
 //     justifyContent: 'center',
 //     backgroundColor: 'red'
 
 // }
 
-export default Header
+export default Header;
