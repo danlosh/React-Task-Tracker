c28eed12752491d0c722aa4e120458e11c6a36c3
Daniel Loshkarev <81983874+danlosh@users.noreply.github.com>
2022-03-21 17:20:01 -0600

    fully functioning task tracker app

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h4 className="version">Version 1.0.0</h4>
+            <p className="paragraph">This Task Tracker App Was Built For Tracking Your Day To Day Tasks And Allow You To Manage Your Time In A More Efficient Way. Allowing You To Add And Delete Tasks As Well As Set Reminders. </p>
+            <a href="/" className="go-back">Go Back</a>
+        </div>
+    )
+}
+
+export default About
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
-      <h1>{title}</h1>
-      <Button onClick={onAdd} color="green" text="ADD" />
+      <h1 className="h1">{title}</h1>
+      <Button onClick={onAdd} color={showAdd ? "red" : "green" } text={showAdd ? "Close" : "Add"} />
     </header>
   );
 };
